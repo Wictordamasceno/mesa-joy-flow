3403b42761e1bb9a603eee7c85ed92f719923510
Wictor Damasceno <damascenowictor@hotmail.com>
2025-12-24 12:53:17 -0400

    Configurações iniciais
    Add Capacitor ao projeto (ios configurado)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.5078e5924feb4f0fb71f08bce3e75f01',
+  appId: 'com.damatech.comanda',
   appName: 'ComandaPro',
-  webDir: 'dist',
-  server: {
-    url: 'https://5078e592-4feb-4f0f-b71f-08bce3e75f01.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
